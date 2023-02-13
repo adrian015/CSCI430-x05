fd6ee5786224e76547baa49b546ebfe74108ebb2
Adrian Arredondo <arredondoadrian015@gmail.com>
2023-02-13 12:11:15 -0800

    added cpp file

---
+++ firstIO.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello! I am slightly changing this to avoid plagerism!\n";
+
+    int number = 0;
+    cout <<"Type a number: ";
+    cin >>number;
+    cout<<number<<"? Awesome choice!\n";
+
+    return 0;
+}
